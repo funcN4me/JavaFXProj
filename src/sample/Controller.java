57a3ea07b79c05b3c442aa9d102c75b03b9d91c8
funcN4me <nazarovnikita776@gmail.com>
2020-11-26 22:33:32 +0400

    Start v2.0

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class Controller {
 
@@ -15,16 +16,25 @@ public class Controller {
     private URL location;
 
     @FXML
+    private Label phraseOfDayLabel;
+
+    @FXML
     private Button button1;
 
     @FXML
+    private Label welcomeLabel;
+
+    @FXML
     void Hello(ActionEvent event) {
-        System.out.println("Pooaoaoadhsdhh");
+        System.out.println("Privet");
     }
 
     @FXML
     void initialize() {
+        assert phraseOfDayLabel != null : "fx:id=\"phraseOfDayLabel\" was not injected: check your FXML file 'pizdec.fxml'.";
         assert button1 != null : "fx:id=\"button1\" was not injected: check your FXML file 'pizdec.fxml'.";
+        assert welcomeLabel != null : "fx:id=\"welcomeLabel\" was not injected: check your FXML file 'pizdec.fxml'.";
+        phraseOfDayLabel.setText(DBFuncs.getPhrase());
 
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("pizdec.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 811, 604));
         primaryStage.show();
     }
 
