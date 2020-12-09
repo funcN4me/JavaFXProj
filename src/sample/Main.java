5044aa1beebe2f8a66d6c94c43f5b998d3872f16
funcN4me <nazarovnikita776@gmail.com>
2020-12-10 00:39:49 +0400

    Completed task

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,19 +10,15 @@ public class Main extends Application {
     static Controller myControllerHandle;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("pizdec.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sceneDiary.fxml"));
         Parent root = loader.load();
         myControllerHandle = (Controller)loader.getController();
 
         Scene scene = new Scene(root, 1047, 627);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Hello, motherfucker");
+        primaryStage.setTitle("Hello, dear!");
         primaryStage.show();
 
-//        Parent root = FXMLLoader.load(getClass().getResource("pizdec.fxml"));
-//        primaryStage.setTitle("Hello World");
-//        primaryStage.setScene(new Scene(root, 811, 610));
-//        primaryStage.show();
     }
 
 
